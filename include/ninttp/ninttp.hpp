4b2bb4a42caded95b25439a9e22ff1e4006312d3
Nicolás Serrano García <serranogarcianicolas@gmail.com>
2026-04-03 18:09:22 +0200

    chore: added final documentation on errors.hpp and added it to the umbrella include

--- include/ninttp/ninttp.hpp
+++ include/ninttp/ninttp.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
+#include "socket/errors.hpp"
+#include "socket/types.hpp"
+#include "socket/interfaces.hpp"
 #include "socket/socket.hpp"
 #include "socket/endpoints.hpp"
-#include "socket/interfaces.hpp"
-#include "socket/types.hpp"
--- include/ninttp/socket/errors.hpp
+++ include/ninttp/socket/errors.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file errors.hpp
+ * @author Nicolás Serrano (serranogarcianicolas@gmail.com)
+ * @brief Defines different error types used on sockets
+ * @version 0.1
+ * @date 2026-04-03
+ * 
+ * @copyright Copyright (c) 2026 Nicolás Serrano García
+ * 
+ */
+
 #pragma once
 
 #include <string>
@@ -8,20 +19,33 @@
 
 namespace ninttp
 {
-    
-    //.msg() can be verbose,so most of the times, if you want to handle the error, you may want to consult the nativeError_ code
-    //which on unix platforms comes from errno, and handle each individually
+    /**
+     * @brief Represents an error in a socket operation
+     */
     struct socketError{
-        socketError(const char* context, std::source_location location = std::source_location::current()) noexcept 
-            : location_(location), context_(context){}
+        /**
+         * @brief Builds a socketError instance from an optional context and the location of the caller
+         * @param context Optional context string
+         * @param location Source location of the caller
+         * @note it is safe to pass @c nullptr to @p context
+         * @details source_location is not specified to be constexpr nor consteval so we can't mark this constructor as such
+         */
+        socketError(const char* context = nullptr, const std::source_location& location = std::source_location::current()) noexcept 
+            : location_(location), context_(context == nullptr ? "" : context){}
 
-        std::string msg() const noexcept{
+        /**
+         * @brief Returns a comprehensive error message combining the location and the context given in construction
+         * @return The message composed from the call-site function and the optional context
+         * @details SocketError::msg() can be verbose. If you prefer to handle the error yourself, you may want to consult the nativeError_ code.
+         * On unix platforms comes from errno, for example.
+         */
+        constexpr std::string msg() const noexcept{
             return location_.function_name() + std::string(": ") + context_;
         }
 
         
-        std::source_location location_;
-        std::string context_;
+        std::source_location location_; //! the source location of the caller
+        std::string context_; //! the context of the error. Can be built from native error codes
 
         //#ifdef OSMACRO
         int native_;
@@ -29,13 +53,25 @@ namespace ninttp
         
     };
 
+    /**
+     * @brief The policy to use when shutting down (parts of) a socket
+     * 
+     * @see toNativeShutdownPolicy
+     * @see SocketBase::shutdown
+     */
     enum class ShutdownPolicy{
-        SHUT_RECEPTIONS,
-        SHUT_TRANSMISSIONS,
-        SHUT_TRANSMISSIONS_AND_RECEPTIONS
+        SHUT_RECEPTIONS, //! Shuts down receptions. This means that the socket that you call this policy on won't be able to receive further information from it's peer
+        SHUT_TRANSMISSIONS, //! Shuts down transmissions. This means that the socket that you call this policy on won't be able to send further information from it's peer
+        SHUT_TRANSMISSIONS_AND_RECEPTIONS //! Does the same as the other two
     };
 
-    int toNativeShutdownPolicy(ShutdownPolicy what){
+    /**
+     * @brief Converts ninttp policy to native policy
+     * 
+     * @param what the policy to convert from
+     * @return constexpr int representing the native code, or -1 if there isn't a match
+     */
+    constexpr int toNativeShutdownPolicy(ShutdownPolicy what){
         switch(what){
             case ShutdownPolicy::SHUT_RECEPTIONS:
                 return SHUT_RD;
@@ -49,39 +85,61 @@ namespace ninttp
     }
 
 
+    /**
+     * @brief Known policy-level errors used when validating library enums and conversions
+     */
     enum class PolicyErrCode{
-        UNKNOWN,
-        UNRECOGNIZED_DOMAIN,
-        UNRECOGNIZED_SERVICE,
-        UNRECOGNIZED_PROTOCOL,
-        UNRECOGNIZED_SHUTDOWN_POLICY
+        UNKNOWN, //! unknown policy error
+        UNRECOGNIZED_DOMAIN, //! the provided domain is not recognized by ninttp
+        UNRECOGNIZED_SERVICE, //! the provided service is not recognized by ninttp
+        UNRECOGNIZED_PROTOCOL, //! the provided protocol is not recognized by ninttp
+        UNRECOGNIZED_SHUTDOWN_POLICY //! the provided shutdown policy is not recognized by ninttp
     };
 
+    /**
+     * @brief Represents a policy conversion/validation error
+     */
     struct PolicyError{
+        /**
+         * @brief Builds a PolicyError from a policy error code and the location of the caller
+         * @param policyError the policy error code
+         * @param location Source location of the caller
+         */
         PolicyError(PolicyErrCode policyError, std::source_location location = std::source_location::current()) noexcept 
             : location_(location), policyError_(policyError) {};
 
-        std::string msg() const noexcept{
+        /**
+         * @brief Returns a comprehensive error message combining location and policy code description
+         * @return The message composed from the caller function and the policy error description
+         */
+        constexpr std::string msg() const noexcept{
             return location_.function_name() + std::string(": ") + strFromPolicyError_(policyError_);
         }
 
-        std::source_location location_;
-        PolicyErrCode policyError_;
+        std::source_location location_; //! the source location of the caller
+        PolicyErrCode policyError_; //! the policy-level error code
 
-        static std::string strFromPolicyError_(PolicyErrCode pErr) noexcept{
-            switch(pErr){
-                case PolicyErrCode::UNRECOGNIZED_DOMAIN:
-                    return "unrecognized domain";
-                case PolicyErrCode::UNRECOGNIZED_SERVICE:
-                    return "unrecognized service";
-                case PolicyErrCode::UNRECOGNIZED_PROTOCOL:
-                    return "unrecognized protocol";
-                case PolicyErrCode::UNRECOGNIZED_SHUTDOWN_POLICY:
-                    return "unrecognized socket shutdown policy";
-                case PolicyErrCode::UNKNOWN:
-                default:
-                    return "unknown policy error";
+        private:
+
+            /**
+             * @brief Converts a policy error code into its human-readable description
+             * @param pErr the policy error code
+             * @return constexpr std::string with the textual description of @p pErr
+             */
+            constexpr static std::string strFromPolicyError_(PolicyErrCode pErr) noexcept{
+                switch(pErr){
+                    case PolicyErrCode::UNRECOGNIZED_DOMAIN:
+                        return "unrecognized domain";
+                    case PolicyErrCode::UNRECOGNIZED_SERVICE:
+                        return "unrecognized service";
+                    case PolicyErrCode::UNRECOGNIZED_PROTOCOL:
+                        return "unrecognized protocol";
+                    case PolicyErrCode::UNRECOGNIZED_SHUTDOWN_POLICY:
+                        return "unrecognized socket shutdown policy";
+                    case PolicyErrCode::UNKNOWN:
+                    default:
+                        return "unknown policy error";
+                }
             }
-        }
     };
 } // namespace ninttp
